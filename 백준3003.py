e2de238a8e2c5bf96965cacc28d7bb3d24371052
DOGYUN <do6065@pusan.ac.kr>
2021-05-03 22:30:16 +0900

    백준3003

---
+++ 백준3003.py
@@ -0,0 +1,9 @@
+
+my_list=  input().split(" ")
+my_list= list(map(int, my_list))
+print(1-my_list[0],end=" ")
+print(1-my_list[1], end=" ")
+print(2-my_list[2], end=" ")
+print(2-my_list[3],end=" ")
+print(2-my_list[4],end=" ")
+print(8-my_list[5],end=" ")
